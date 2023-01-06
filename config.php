c461bad46667eabb0768d0742f84e7d76d8ea90e
Jhimy Ferrari <Souzaferrarik@gmail.com>
2023-01-06 09:07:00 -0300

    Criação de Pasta class

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($clas_name) {
-    $filename = $clas_name . ".php";
+    $filename ="class".DIRECTORY_SEPARATOR. $clas_name . ".php";
 
     if (file_exists($filename)) {
         require_once($filename);
